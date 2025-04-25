b9350e88e0a792dc37c47fb6514781a4ac415460
luq-sham <luqman.shamsul10@gmail.com>
2025-04-25 15:39:06 +0800

    commit again

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,9 @@ bootstrapApplication(AppComponent, {
           }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           }),
   ],
 });
